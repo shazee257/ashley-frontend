8b14a36b7f77c9c357e77137abfa7fa3bde3d02b
adnanabrar786 <adnan1245abrar@gmail.com>
2022-10-17 09:54:35 -0700

    star and link issue resloved

--- components/ProductCard.jsx
+++ components/ProductCard.jsx
@@ -25,35 +25,38 @@ const ProductCard = ({ cardProduct }) => {
   };
 
   return (
-    <Link href={`/products/details/${cardProduct.slug}`}>
-      <a className={product.products_card}>
-        <div className={product.heart}>
-          <h4 className={product.icon}>
-            <AiOutlineHeart />
-          </h4>
-          <h4 className={product.display}>Add to Wishlist</h4>
-        </div>
-        <div className={product.card_image}>
-          <Image
-            src={`${process.env.NEXT_PUBLIC_uploadURL}/products/${image}`}
-            alt="bed" layout="fill" objectFit="cover"
-          />
-          {/* <div className={product.display}>
+    <div className={product.products_card}>
+      <div className={product.heart}>
+        <h4 className={product.icon}>
+          <AiOutlineHeart />
+        </h4>
+        <h4 className={product.display}>Add to Wishlist</h4>
+      </div>
+      <Link href={`/products/details/${cardProduct.slug}`}>
+        <a className={product.card_details}>
+          <div className={product.card_image}>
+            <Image
+              src={`${process.env.NEXT_PUBLIC_uploadURL}/products/${image}`}
+              alt="bed" layout="fill" objectFit="cover"
+            />
+            {/* <div className={product.display}>
             <Image
               src={`${process.env.NEXT_PUBLIC_uploadURL}products/${imageHover}`}
               alt="bed" layout="fill" objectFit="cover"
             />
           </div> */}
-        </div>
-        <div className={product.card_info}>
-          <h4>{cardProduct.title}</h4>
-          <div className={product.ratings}>
-            <ReactStars {...options} />
           </div>
-          <p>$ {minPrice} - $ {maxPrice}</p>
-        </div>
-      </a>
-    </Link>
+          <div className={product.card_info}>
+            <h4>{cardProduct.title}</h4>
+            <div className={product.ratings}>
+              <ReactStars {...options} />
+            </div>
+            <p>$ {minPrice} - $ {maxPrice}</p>
+          </div>
+        </a>
+      </Link>
+    </div>
+
   );
 };
 
