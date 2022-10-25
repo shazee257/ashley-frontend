5e26222044fda25a9f4642f1ac40b20df24b4884
adnanabrar786 <adnan1245abrar@gmail.com>
2022-10-25 05:31:02 -0700

    wishlist work

--- pages/wishlist.jsx
+++ pages/wishlist.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 
 import { useSelector, useDispatch } from "react-redux";
@@ -12,11 +12,33 @@ import wishlistimg from "./assets/fur12.jpg";
 import { AiOutlineDelete, AiOutlineHeart } from "react-icons/ai";
 
 const Wishlist = () => {
+
+  const [wishlistData, setWishlistData] = useState([]);
   const wishList = useSelector(selectWishlist);
   const dispatch = useDispatch();
   const deleteHandler = (id) => {
     dispatch(removeToWishlist(id));
   };
+
+  function setWishlistPrices(payload) {
+    const data = payload.map((product) => {
+      const prices = product.variants.map((variant) => variant.sale_price);
+      const minPrice = Math.min(...prices);
+      const maxPrice = Math.max(...prices);
+      return { ...product, minPrice, maxPrice }
+    })
+    // return data;
+    setWishlistData(data);
+  }
+  console.log(wishlistData, "!!!!!!!");
+
+  useEffect(() => {
+    setWishlistPrices(wishList.data);
+  }, [])
+
+
+  // console.log(wishlistData, "!!!!!!!!!!!!!!!");
+
   return (
     <div className={styles.wishlist_wrapper}>
       <div className={styles.wishlist_heading}>
@@ -26,21 +48,41 @@ const Wishlist = () => {
         </h1>
       </div>
       <div className={styles.wishlist_cards_wrapper}>
-        {wishList?.map((item) => (
-          <div className={styles.wishlist_card} key={item._id}>
-            <div className={styles.wishlist_card_img}>
+        {wishlistData?.map((item) => (
+          <div className={styles.wishlist_card}>
+            <div className={styles.wishlist_card_img_wrapper}>
+              <div className={styles.wishlist_card_img}>
+                <Image
+                  // src={`${process.env.NEXT_PUBLIC_uploadURL}/products/${item.image}`}
+                  src={`${process.env.NEXT_PUBLIC_uploadURL}/products/${item.variants[0].features[0].images[0]}`}
+                  alt="wishlist-img"
+                  layout="fill"
+                // objectFit="contain"
+                />
+              </div>
+              <div className={styles.wishlist_prices}>
+                <h3>$ {item.maxPrice} </h3>
+                 <h3>-</h3>
+                <h3>$ {item.minPrice} </h3>
+              </div>
+            </div>
+            {/* <div className={styles.wishlist_card_img}>
               <Image
-                src={`${process.env.NEXT_PUBLIC_uploadURL}/products/${item.image}`}
+                // src={`${process.env.NEXT_PUBLIC_uploadURL}/products/${item.image}`}
+                src={`${process.env.NEXT_PUBLIC_uploadURL}/products/${item.variants[0].features[0].images[0]}`}
                 alt="wishlist-img"
                 layout="fill"
                 objectFit="contain"
               />
-            </div>
+
+            </div> */}
+
+
             <div className={styles.wishlist_card_info}>
-              {console.log(`/${process.env.NEXT_PUBLIC_uploadURL}/products/${item.image}`)}
+              {console.log(`/${process.env.NEXT_PUBLIC_uploadURL}/products/${item.variants[0].features[0].images[0]}`)}
               <h3> {item.title} </h3>
-              <h6>{item.size}</h6>
-              <h6>{item.color} </h6>
+              {/* <h6>{item.variants[0].size}</h6> */}
+              {/* <h6>{item.color} </h6> */}
               <h6>In Stock</h6>
               <div className={styles.btn}>
                 <button
