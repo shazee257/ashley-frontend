68e06833e69c1b9008970ff1e48139ea82493cf4
adnanabrar786 <adnan1245abrar@gmail.com>
2022-11-03 03:31:14 -0700

    hover cart cartnew learn how

--- components/HoverCart.jsx
+++ components/HoverCart.jsx
@@ -43,49 +43,51 @@ const HoverCart = () => {
   return (
     <div className={cart.main}>
       {selectCartDetail?.length === 0 ? (
-        <h2>Cart is Empty</h2>
+        <h2 className={cart.empty_card_heading}>Cart is Empty</h2>
       ) : (
-        selectCartDetail?.map((item) => (
-          <div className={cart.card} key={item.sku}>
-            <div className={cart.card_img_info}>
-              <div className={cart.pic}>
-                <Image
-                  src={`${process.env.NEXT_PUBLIC_uploadURL}/products/${item.image}`}
-                  alt={item.title}
-                  layout="fill"
-                  objectFit="cover"
-                />
-              </div>
-              <div className={cart.info}>
-                <div className={cart.name_color_size}>
-                  <h6>{item.title}</h6>
-                  <p>Code : <span className={cart.bold_color_size}>{item.sku}</span></p>
-                  <p>Color : <span className={cart.bold_color_size}>{item.color}</span></p>
-                  <p>Size : <span className={cart.bold_color_size}>{item.size}</span> </p>
+        <div  className={cart.hover_cart}>
+          {selectCartDetail?.map((item) => (
+            <div className={cart.card} key={item.sku}>
+              <div className={cart.card_img_info}>
+                <div className={cart.pic}>
+                  <Image
+                    src={`${process.env.NEXT_PUBLIC_uploadURL}/products/${item.image}`}
+                    alt={item.title}
+                    layout="fill"
+                    objectFit="cover"
+                  />
                 </div>
-                <div className={cart.qty_price}>
-                  <div className={cart.qty}>
-                    <p className={cart.qty_name}>Qty :</p>
-                    <div className={cart.qty_value}>
-                      <p onClick={() => decQty(item.sku)}>-</p>
-                      <p>{item.quantity}</p>
-                      <p onClick={() => incQty(item.sku)}>+</p>
-                    </div>
-                    <p className={cart.update_btn_div}>
-                      <button className={cart.update_btn}>
-                        Update
-                      </button>
-                    </p>
-                  </div>
+                <div className={cart.info}>
                   <div className={cart.name_color_size}>
-                    <p>Price : <span className={cart.bold_color_size}>${item.price}</span></p>
-                    <p>Subtotal : <span className={cart.bold_color_size}>${item.total}</span> </p>
+                    <h6>{item.title}</h6>
+                    <p>Code : <span className={cart.bold_color_size}>{item.sku}</span></p>
+                    <p>Color : <span className={cart.bold_color_size}>{item.color}</span></p>
+                    <p>Size : <span className={cart.bold_color_size}>{item.size}</span> </p>
                   </div>
-                  {/* <div className={cart.price}>
+                  <div className={cart.qty_price}>
+                    <div className={cart.qty}>
+                      <p className={cart.qty_name}>Qty :</p>
+                      <div className={cart.qty_value}>
+                        <input type="text" className={cart.input_qty_value}/>
+                        {/* <p onClick={() => decQty(item.sku)}>-</p>
+                        <p>{item.quantity}</p>
+                        <p onClick={() => incQty(item.sku)}>+</p> */}
+                      </div>
+                      <p className={cart.update_btn_div}>
+                        <button className={cart.update_btn}>
+                          Update
+                        </button>
+                      </p>
+                    </div>
+                    <div className={cart.name_color_size}>
+                      <p>Price : <span className={cart.bold_color_size}>${item.price}</span></p>
+                      <p>Subtotal : <span className={cart.bold_color_size}>${item.total}</span> </p>
+                    </div>
+                    {/* <div className={cart.price}>
                     <span> $ {cartDetail.price}</span> <span className={cart.diff_line}>-</span>
                     <span> $ {cartDetail.price * cartDetail.quantity}</span>
                   </div> */}
-                  {/* <div className={cart.price}>
+                    {/* <div className={cart.price}>
                     <h6>Item Price : </h6>
                     <h4> $ {cartDetail.price}</h4>
                   </div>
@@ -93,26 +95,32 @@ const HoverCart = () => {
                     <h6>Item Total : </h6>
                     <h3> $ {cartDetail.price * cartDetail.quantity}</h3>
                   </div> */}
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className={cart.remove_btn}>
-              <button onClick={() => removeCartItemHandler(loginData.user_id, item._id)}>
-                Remove Item
-              </button>
-              {/* <button className={cart.ChechOut_btn} >
+              <div className={cart.remove_btn}>
+                <button onClick={() => removeCartItemHandler(loginData.user_id, item._id)}>
+                  Remove Item
+                </button>
+                {/* <button className={cart.ChechOut_btn} >
                  Chechout
               </button> */}
+              </div>
             </div>
+          ))}
+          <div className={cart.ChechOut_btn_div}>
+            <button className={cart.ChechOut_btn} >
+              Chechout
+            </button>
           </div>
-        ))
+        </div>
       )}
 
-      <div className={cart.ChechOut_btn_div}>
+      {/* <div className={cart.ChechOut_btn_div}>
         <button className={cart.ChechOut_btn} >
           Chechout
         </button>
-      </div>
+      </div> */}
     </div>
   );
 };
--- components/ProductCard.jsx
+++ components/ProductCard.jsx
@@ -66,6 +66,7 @@ const ProductCard = ({ cardProduct }) => {
     value: cardProduct.rating,
   };
 
+  const [showModal, setShowModal] = React.useState(false);
   return (
     <div className={product.products_card}>
       <div className={product.heart} onClick={() => wishlistHandler(cardProduct._id)}>
@@ -74,23 +75,86 @@ const ProductCard = ({ cardProduct }) => {
         </h4>
         <h4 className={product.display}>Add to Wishlist</h4>
       </div>
-      <Link href={`/products/details?slug=${cardProduct.slug}`}>
-        <a className={product.card_details}>
-          <div className={product.card_image}>
-            <Image
-              src={`${process.env.NEXT_PUBLIC_uploadURL}/products/${image}`}
-              alt="bed" layout="fill" objectFit="cover"
-            />
-          </div>
-          <div className={product.card_info}>
-            <h4>{cardProduct.title}</h4>
-            <div className={product.ratings}>
-              <ReactStars size={24} {...options} />
+      {/* <Link href={`/products/details?slug=${cardProduct.slug}`}> */}
+      <div className={product.card_details}>
+        <Link href={`/products/details?slug=${cardProduct.slug}`}>
+          <a className={product.hanker_wrapper}>
+            <div className={product.card_image}>
+              <Image
+                src={`${process.env.NEXT_PUBLIC_uploadURL}/products/${image}`}
+                alt="bed" layout="fill" objectFit="cover"
+              />
             </div>
-            <p>$ {minPrice} - $ {maxPrice}</p>
-          </div>
-        </a>
-      </Link>
+            <div className={product.card_info}>
+              <h4>{cardProduct.title}</h4>
+              <div className={product.ratings}>
+                <ReactStars size={24} {...options} />
+              </div>
+              <p className={product.max_min_prize}>${minPrice} - ${maxPrice}</p>
+              {/* <p className={product.year_schedule}>or $84/mo w/ 12 mos special financing</p> */}
+              <p className={product.year_schedule}>or ${Math.ceil(minPrice / 12)} or  ${Math.ceil(maxPrice / 12)} w/ 12 mos special financing</p>
+            </div>
+          </a>
+        </Link>
+        <div className={product.learn_how}>
+          <p className={product.policy} onClick={() => setShowModal(true)}>Leran How</p>
+          {showModal ? (
+            <>
+              <div
+                className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+              >
+                {/* <div className="relative w-auto my-6 mx-auto max-w-sm"> */}
+                <div className="relative w-auto my-6 mx-auto max-w-sm">                   {/*content*/}
+                  <div className=" border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                    {/*header*/}
+                    <div className="flex items-start justify-between p-3 text-center  border-b border-solid border-slate-200 rounded-t">
+                      <h3 className="text-lg font-semibold text-center">
+                        Promotional Period: 6 Months
+                      </h3>
+                    </div>
+                    {/*body*/}
+                    <div className="h-72 relative px-3 flex-auto overflow-y-scroll">
+                      <div className="my-2 text-slate-500 leading-relaxed text-sm">
+                        <p className="font-bold my-2 text-black">
+                          Don’t have an Ashley Advantage™ credit card? See if you prequalify or apply now.
+                        </p>
+                        <p className="my-3 text-black">
+                          The Suggested Equal Monthly Payment shown may be greater than the required minimum monthly payment that will be on your billing statement when you use the 6 month promotional financing offer. This estimated payment : equals the promotional purchase amount divided by 6(excluding taxes and shipping charges) rounded to the next highest whole dollar,*Would pay off the promotional purchase amount within 6 months, but only if there are no other balances on your account at any time during the 6 month promotional period and you make your payments on time. Your total payments will equal the amount of the promotional purchase amount. If the promotional purchase amount is not paid in full within 6 months, interest will be charged to your account from the purchase date and your total payments will be greater than the amount of the promotional purchase amount.
+                        </p>
+                        <p className="my-3 text-black">
+                          Important: The information about the Suggested Equal Monthly Payment shown assumes the following promotional financing offer is applied to the purchase:
+                        </p>
+                        <p className="my-3 text-black">
+                          No Interest if paid in full within 6 months†††
+                        </p>
+                        <p className="my-3 text-black">
+                          On qualifying purchases with your Ashley Advantage credit card. Interest will be charged to your account from the purchase date if the promotional purchase is not paid in full within 16months. Minimum monthly payments required. $150 minimum purchase required.
+                        </p>
+                        <p className="my-3 text-black">
+                          †††Offer applies only to single-receipt qualifying purchases. No interest will be charged on the promo purchase if you pay the promo purchase amount in full within 6 Months. If you do not, interest will be charged on the promo purchase from the purchase date. The required minimum monthly payments may or may not pay off purchase by end of promotional period. Regular account terms apply to non-promotional purchases and, after promotion ends, to the remaining promotional balance. For new accounts: Purchase APR is 29.99%; Minimum Interest Charge is $2. Existing cardholders should see their credit card agreement for their applicable terms. Subject to credit approval. We reserve the right to discontinue or alter the terms of this offer any time.
+                        </p>
+                      </div>
+                    </div>
+                    {/*footer use as a background color*/}
+                    <div className="flex items-center justify-end py-2 border-t border-solid border-slate-200 rounded-b">
+                      <button
+                        className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                        type="button"
+                        onClick={() => setShowModal(false)}
+                      >
+                        Close
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+            </>
+          ) : null}
+
+        </div>
+      </div>
+      {/* </Link> */}
     </div>
 
   );
