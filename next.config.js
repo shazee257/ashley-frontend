bb7e8555df9c98a11806cac7d02d0fce5b4aef7a
Muhammad Shahzad <shazee257@gmail.com>
2022-12-24 10:23:48 +0500

    meta httpEquiv

--- next.config.js
+++ next.config.js
@@ -1,13 +1,3 @@
-const securityHeaders = []
-
-const ContentSecurityPolicy = `
-  default-src 'self';
-  script-src 'self';
-  child-src example.com;
-  style-src 'self' example.com;
-  font-src 'self';  
-`
-
 module.exports = {
   reactStrictMode: true,
   // swcMinify: true,
@@ -17,32 +7,4 @@ module.exports = {
       "54.145.81.48",
     ],
   },
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: securityHeaders,
-      },
-      {
-        key: 'X-XSS-Protection',
-        value: '1; mode=block'
-      },
-      {
-        key: 'X-Frame-Options',
-        value: 'SAMEORIGIN'
-      },
-      {
-        key: 'X-Content-Type-Options',
-        value: 'nosniff'
-      },
-      // {
-      //   key: 'Referrer-Policy',
-      //   value: 'same-origin'
-      // },
-      {
-        key: 'Content-Security-Policy',
-        value: ContentSecurityPolicy.replace(/\n/g, '')
-      }
-    ]
-  }
 }
--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        <meta httpEquiv="Content-Security-Policy" />
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
           rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
           crossOrigin="anonymous">
