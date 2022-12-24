2e5f6a2ec99e0bf0ff4dbd84ca269ef8abc2be7a
Muhammad Shahzad <shazee257@gmail.com>
2022-12-24 09:44:10 +0500

    update

--- next.config.js
+++ next.config.js
@@ -1,12 +1,29 @@
-/** @type {import('next').NextConfig} */
+const securityHeaders = []
+
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self';
+  child-src example.com;
+  style-src 'self' example.com;
+  font-src 'self';  
+`
+
 module.exports = {
   reactStrictMode: true,
   // swcMinify: true,
   images: {
     domains: [
       "localhost",
-      "api-mecca.vercel.app",
       "54.145.81.48",
     ],
   },
-};
+  async headers() {
+    return [
+      {
+        key: 'Content-Security-Policy',
+        value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim()
+      },
+    ]
+  }
+}
+
