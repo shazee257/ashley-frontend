e58d35006ca116f31c92afacd1bce036de1d1536
adnanabrar786 <adnan1245abrar@gmail.com>
2023-01-02 17:35:19 -0800

    learn how remove in product page

--- components/ProductCard.jsx
+++ components/ProductCard.jsx
@@ -96,23 +96,20 @@ const ProductCard = ({ cardProduct }) => {
             </div>
           </a>
         </Link>
-        <div className={product.learn_how}>
+        {/* <div className={product.learn_how}>
           <p className={product.policy} onClick={() => setShowModal(true)}>Learn How</p>
           {showModal ? (
             <>
               <div
                 className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
               >
-                {/* <div className="relative w-auto my-6 mx-auto max-w-sm"> */}
-                <div className="relative w-auto my-6 mx-auto max-w-sm">                   {/*content*/}
+                <div className="relative w-auto my-6 mx-auto max-w-sm">               
                   <div className=" border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-                    {/*header*/}
                     <div className="flex items-start justify-between p-3 text-center  border-b border-solid border-slate-200 rounded-t">
                       <h3 className="text-lg font-semibold text-center">
                         Promotional Period: 6 Months
                       </h3>
                     </div>
-                    {/*body*/}
                     <div className="h-72 relative px-3 flex-auto overflow-y-scroll">
                       <div className="my-2 text-slate-500 leading-relaxed text-sm">
                         <p className="font-bold my-2 text-black">
@@ -135,7 +132,6 @@ const ProductCard = ({ cardProduct }) => {
                         </p>
                       </div>
                     </div>
-                    {/*footer use as a background color*/}
                     <div className="flex items-center justify-end py-2 border-t border-solid border-slate-200 rounded-b">
                       <button
                         className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
@@ -152,9 +148,8 @@ const ProductCard = ({ cardProduct }) => {
             </>
           ) : null}
 
-        </div>
+        </div> */}
       </div>
-      {/* </Link> */}
     </div>
 
   );
